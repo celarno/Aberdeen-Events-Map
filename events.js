0da9761a3ec351ac6067df632c160c0e4e774450
celarno <marcel-arnold@gmx.de>
2018-04-20 14:35:37 +0100

    Add files via upload

--- events.js
+++ events.js
@@ -260,13 +260,10 @@ function setMarker(title, cat, scat, website, events, location, fb, rating){
         stars = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>';
     }
 
-    stars = '<div class="stars" style="float:right;" onclick="changeStars();">'+ stars + '</div>';
-
-    function changeStars(){
-        $('#add').hide();
-        $('#rating').show();
-        $('#rating_name').html(title);
-    };
+    stars = '<div class="stars" style="float:right;" onclick="$(\'#add\').hide();\n' +
+        '        $(\'#rating\').show();\n' +
+        '        $(\'#rating_name\').html(' + title+ ');">'+ stars + '</div>';
+    
 
     var close = '<a href="#" style="margin-top:-1em;color:black;padding:0 3px;' +
         'text-decoration:none;float:right;" onclick="$(\'#info\').hide();"><i class="fas fa-times"></i></a><br>';
